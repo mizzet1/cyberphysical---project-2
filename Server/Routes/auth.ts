09bc882b9829f5ed31d85aa7b39db8d304edafcc
davidespada99 <spadads@gmail.com>
2025-01-07 15:40:06 +0100

    generateM2() done

--- Server/Routes/auth.ts
+++ Server/Routes/auth.ts
@@ -3,17 +3,16 @@ import express from "express";
 export const authRouter = express.Router();
 
 
-authRouter.get("/:m1", async (req, res) => {
+authRouter.get("/:m1", async (req: any, res: any) => {
     const deviceId = req?.body?.deviceId;
     const sessionId = req?.body?.sessionId;
     if (checkDeviceId(deviceId)){
         const M2 = generateM2(); // if valid Device ID --> generate message M2
+        return res.status(200).json(M2);
     }
     else{
-        return res.status(401).send("Invalid device Id"); // else, return 401 Unauthorized status code
+        return res.status(401).send("Invalid Device Id"); // else, return 401 Unauthorized status code
     }
-
-
 });
 
 //Check if DeviceId of the request is known to server
@@ -28,13 +27,18 @@ function checkDeviceId(deviceId: String): boolean {
 }
 
 function generateM2(){
-    //generate r1
-    //generate C1
+    const r1 = generateR1();
+    const C1 = generateC1();
+    const M2 = {
+        r1: r1,
+        C1: C1
+    }
+    return M2;
 }
 
 function generateC1(): number[] {
     const c1: number[] = [];
-    // Generates p, a random number from 2 and 7 (i.e. at least 2 keys, at most n-1 keys). 
+    // Generates p, a random number from 2 and 7 (i.e. at least 2 keys, at most 7 (n-1) keys). 
     // p determines the number of keys indeces for C1
     const p: number = Math.floor(Math.random() * 6) + 2; 
 
@@ -46,5 +50,9 @@ function generateC1(): number[] {
     }
     return c1;
   }
-  checkDeviceId
+
+  function generateR1(): number{
+    return Math.floor(Math.random() * 1000);
+  }
+
   
